51ec07394a1c7fd1fac8500fb5eac258b82b0a8c
Alberto Garcia <alberto.garcia0@walmart.com>
2021-06-08 15:33:20 -0500

    Initial Commit

---
+++ SwiftUICoreDataExample/SwiftUICoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoreDataExampleApp.swift
+//  SwiftUICoreDataExample
+//
+//  Created by Alberto Garcia on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
