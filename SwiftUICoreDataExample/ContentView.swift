da9aabbe861f616a0a9ea3841115965fcf2d7f89
Alberto Garcia <alberto.garcia0@walmart.com>
2021-06-08 23:08:25 -0500

    Designed 'ContentView' and add logic to create new elements to the list

--- SwiftUICoreDataExample/ContentView.swift
+++ SwiftUICoreDataExample/ContentView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    @State var example = [
+        "title1",
+        "title2",
+        "title3",
+        "title4",
+        "title5"
+    ]
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        NavigationView {
+            List {
+                ForEach(example, id: \.self) { text in
+                    Text(text)
+                }
+                .onDelete(perform: { indexSet in
+                    print("Delete index: \(indexSet)")
+                })
+                
+            } //: List
+            .toolbar {
+                Button(action: {
+                    example.append("title6")
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle("Reminders")
+        } // NavigationView
+    } //: body
 }
+
+// MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
