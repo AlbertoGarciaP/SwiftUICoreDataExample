9e8286575ec523a5fd5d70d69c60ac9ae4f5e86e
Alberto Garcia <alberto.garcia0@walmart.com>
2021-06-08 23:28:58 -0500

    Add 'NewReminderView'

---
+++ SwiftUICoreDataExample/NewReminderView.swift
@@ -0,0 +1,65 @@
+//
+//  NewReminderView.swift
+//  SwiftUICoreDataExample
+//
+//  Created by Alberto Garcia on 08/06/21.
+//
+
+import SwiftUI
+
+struct NewReminderView: View {
+    
+    // MARK: - Properties
+    
+    @State private var reminder: String = ""
+    @Binding var isShowing: Bool
+    
+    private var isButtonDisabled: Bool {
+        reminder.isEmpty
+    }
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                TextField("New reminder", text: $reminder)
+                    .foregroundColor(.blue)
+                    .font(.system(size: 24, weight: .bold, design: .rounded))
+                    .padding()
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Spacer()
+                    Text("Save")
+                        .font(.system(size: 24, weight: .bold, design: .rounded))
+                    Spacer()
+                })
+                .padding()
+                .foregroundColor(.white)
+                .background(isButtonDisabled ? Color.blue : Color.pink)
+                .cornerRadius(10)
+            } //: VStack
+            .padding(.horizontal)
+            .padding(.vertical, 20)
+            .background(Color.white)
+            .cornerRadius(16)
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
+            .frame(maxWidth: 640)
+        } //: VStack
+        .padding()
+    }
+}
+
+// MARK: - Preview
+
+struct NewReminderView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewReminderView(isShowing: .constant(true))
+            .background(Color.gray.edgesIgnoringSafeArea(.all))
+    }
+}
