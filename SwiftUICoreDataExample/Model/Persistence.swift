c3333a3a598100970a587f5b7c00c288b12df86c
Alberto Garcia <alberto.garcia0@walmart.com>
2021-06-09 12:55:28 -0500

    Add 'SwiftUICoreDataExample' data model and the 'PersistenceController'

---
+++ SwiftUICoreDataExample/Model/Persistence.swift
@@ -0,0 +1,27 @@
+//
+//  Persistence.swift
+//  SwiftUICoreDataExample
+//
+//  Created by Alberto Garcia on 09/06/21.
+//
+
+import CoreData
+
+struct PersistenceController {
+    
+    static let shared = PersistenceController()
+    
+    let container: NSPersistentContainer
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "SwiftUICoreDataExample")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
